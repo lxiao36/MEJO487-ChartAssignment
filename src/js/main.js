d1235e02c933d46831627947f0ccf45a140a8575
lxiao36 <lxiao3@ad.unc.edu>
2023-10-11 15:33:16 -0400

    efficient js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const graphOne = document.getElementById("graph-1");
 const graphTwo = document.getElementById("graph-2");
 const graphThree = document.getElementById("graph-3");
 const graphFour = document.getElementById("graph-4");
+
 const data1 = {
   labels: [
     "Red",
@@ -15,89 +16,33 @@ const data1 = {
   ],
   data: [12, 19, 3, 5, 2, 3, 5, 5],
 };
-console.log(data1);
 
-new Chart(graphOne, {
-  type: "bar",
-  data: {
-    labels: data1.labels,
-    datasets: [
-      {
-        label: "# of Votes",
-        data: data1.data,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    indexAxis: "y",
-    scales: {
-      y: {
-        beginAtZero: true,
+const chartTypes = ["bar", "polarArea", "line", "pie"];
+const graphElements = [graphOne, graphTwo, graphThree, graphFour];
+
+function createChart(element, chartType, chartData) {
+  new Chart(element, {
+    type: chartType,
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: chartData.data,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
-  },
-});
+  });
+}
 
-new Chart(graphTwo, {
-  type: "bar",
-  data: {
-    labels: data1.labels,
-    datasets: [
-      {
-        label: "# of Votes",
-        data: data1.data,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
-
-new Chart(graphThree, {
-  type: "line",
-  data: {
-    labels: data1.labels,
-    datasets: [
-      {
-        label: "# of Votes",
-        data: data1.data,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
-
-new Chart(graphFour, {
-  type: "pie",
-  data: {
-    labels: data1.labels,
-    datasets: [
-      {
-        label: "# of Votes",
-        data: data1.data,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+for (let i = 0; i < chartTypes.length; i++) {
+  createChart(graphElements[i], chartTypes[i], data1);
+}
